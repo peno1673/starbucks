e9381611ba8a4e1159d92a56fbb0b1a1d6f52c5d
peno1673 <kimam44@naver.com>
2023-02-16 22:39:32 +0900

    '2023-02-16'

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', () => {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', () => {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', () => {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle( () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300))
+// _.throttle(함수, 시간)
